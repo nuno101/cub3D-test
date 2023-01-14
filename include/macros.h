e79c611dfbd297f87ebb2ffa3665aa66578cf23f
Nuno <nlouro@student.42heilbronn.de>
2023-01-14 10:42:42 +0100

    Use enum to define error codes and shift them to not colide with the OS error codes

--- include/macros.h
+++ include/macros.h
@@ -6,7 +6,7 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 21:02:43 by ezpiro-m          #+#    #+#             */
-/*   Updated: 2023/01/12 22:05:16 by jjesberg         ###   ########.fr       */
+/*   Updated: 2023/01/14 10:41:39 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,26 @@
 
 # include "cub.h"
 
-# define ARG_ERROR			11
-# define FILE_ERROR			12
-# define INVALID_MAP		13
-# define MALLOC_ERROR		14
-# define DIR_ERROR			15
-# define COLOUR_ERROR		16
-# define MAP_ERROR			17
-# define MLX_ERROR			18
-# define PNG_ERROR      	19
-# define TEX_PATH_ERROR		20
-# define PLAYER_ERROR		21
-# define P_SURROUNDED_ERROR	22
-# define W_SURROUNDED_ERROR	23
+/*
+ * define cub3d specific errors
+ * start at 256 to not overwrite OS error codes
+ */
+enum	e_cub3d_errors
+{
+	ARG_ERROR = 256,
+	FILE_ERROR,
+	INVALID_MAP,
+	MALLOC_ERROR,
+	DIR_ERROR,
+	COLOUR_ERROR,
+	MAP_ERROR,
+	MLX_ERROR,
+	PNG_ERROR,
+	TEX_PATH_ERROR,
+	PLAYER_ERROR,
+	P_SURROUNDED_ERROR,
+	W_SURROUNDED_ERROR,
+}	t_error;
 
 #define NO 101
 #define SO 102
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 17:53:21 by ezpiro-m          #+#    #+#             */
-/*   Updated: 2023/01/13 18:01:54 by jjesberg         ###   ########.fr       */
+/*   Updated: 2023/01/13 19:40:03 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 /*
  * verify a single map filename is passed as parameter
  * initialise data from filename
- * TODO: Julian, I added free(data) which I think was missing. Pls check!  
  */
 int main(int argc, char **argv)
 {
