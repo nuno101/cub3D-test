61c54f02735625109a4cd50c321f05f0a35ee34b
Nuno <nlouro@student.42heilbronn.de>
2023-01-14 23:42:07 +0100

    Simplify definition of texture macros and link retunr value of find_path_type to NO to avoid bugs

--- parser/checks.c
+++ parser/checks.c
@@ -6,7 +6,7 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 02:23:03 by jjesberg          #+#    #+#             */
-/*   Updated: 2023/01/14 10:45:33 by nlouro           ###   ########.fr       */
+/*   Updated: 2023/01/14 23:40:32 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,5 @@ int	find_path_type(char *s)
 			return (WE);
 		i++;
 	}
-	return (0);
+	return (NO - 1);
 }
