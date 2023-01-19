59473b53bf564e0e57453185f29f535095e7e453
Nuno <nlouro@student.42heilbronn.de>
2023-01-19 15:34:46 +0100

    Trying to add a test example

---
+++ tests/test_colours.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>   // used for printf
+#include <stdlib.h> // used for malloc
+#include "libft.h"
+#include "../parser/colours.c"
+
+int	main(void)
+{
+	printf("Hello\n");
+	return (0);
+}
