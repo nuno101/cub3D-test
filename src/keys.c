28e2d76db97ad6f0dea07f547a68602eb61b238c
Nuno Louro <nlouro@student.42heilbronn.de>
2023-01-20 15:36:30 +0100

    Replace exit with mlx_terminate

--- src/keys.c
+++ src/keys.c
@@ -6,7 +6,7 @@
 /*   By: jjesberg <j.jesberger@heilbronn.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 21:16:40 by jjesberg          #+#    #+#             */
-/*   Updated: 2023/01/20 11:50:21 by jjesberg         ###   ########.fr       */
+/*   Updated: 2023/01/20 15:34:39 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ void	cub_keys(mlx_key_data_t key, void *param)
 	printf("key = %i\n", key.key);
 	if (mlx_is_key_down(cub->mlx, MLX_KEY_ESCAPE))
 	{
-		clean_mlx(cub);
-		free_data(cub->d);
-		free_cub(cub);
-		printf("Exit cub3d\n");
-		exit (0);
+		printf("cub_keys: Escape pressed!\n");
+		//clean_mlx(cub);
+		//free_data(cub->d);
+		//free_cub(cub);
+		mlx_terminate(cub->mlx);
 	}
 }
