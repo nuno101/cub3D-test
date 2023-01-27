e639a79ddb28897a594dc1f67c71ef4163bbf339
Nuno <nlouro@student.42heilbronn.de>
2023-01-27 10:44:06 +0100

    Add debug code to show keys pressed and first movement placeholder

--- src/keys.c
+++ src/keys.c
@@ -6,7 +6,7 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 21:16:40 by jjesberg          #+#    #+#             */
-/*   Updated: 2023/01/26 15:54:25 by jjesberg         ###   ########.fr       */
+/*   Updated: 2023/01/27 10:42:58 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,25 @@ void	resize_screen(int32_t x, int32_t y, void *param)
 }
 
 /*
- * print key atm (TODOO implement movement [W|A|S|D] or arrows or both
- * maybe Menu/Pause
- * 
-*/
+ * TODO: prepare image for display in new position
+ * may need several images to reach the new postion in smal steps
+ */
+void	move_fwd(void)
+{
+	printf("TODO: move forward");
+}
+
+/*
+ * TODO implement movement [W|A|S|D] and look to side [arrows]
+ * key codes:
+ * ESC 256
+ * W 87 - move forward
+ * A 65 - move left
+ * S 83 - move back
+ * D 68 - move right
+ * Left-arrow 263 - look left
+ * Right-arrow 262
+ */
 void	cub_keys(mlx_key_data_t key, void *param)
 {
 	t_cub	*cub;
@@ -42,4 +57,7 @@ void	cub_keys(mlx_key_data_t key, void *param)
 		mlx_close_window(cub->mlx);
 		mlx_delete_image(cub->mlx, cub->image);
 	}
+	else if (key.key == 87)
+		move_fwd();
+	printf("DEBUG: key code: %i\n", key.key);
 }
