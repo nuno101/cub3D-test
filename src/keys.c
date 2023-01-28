543573169cd62b6f44a8dae39116b9930f8f20d5
jjesberg <jjesberg@student.42heilbronn.de>
2023-01-28 15:38:14 +0100

    try movement

--- src/keys.c
+++ src/keys.c
@@ -6,7 +6,7 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 21:16:40 by jjesberg          #+#    #+#             */
-/*   Updated: 2023/01/26 15:54:25 by jjesberg         ###   ########.fr       */
+/*   Updated: 2023/01/28 15:35:54 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,51 @@ void	cub_keys(mlx_key_data_t key, void *param)
 		mlx_close_window(cub->mlx);
 		mlx_delete_image(cub->mlx, cub->image);
 	}
+	if (key.key == 87) 
+	{
+		if (cub->ray->wall_distance >= 1)
+		{
+			cub->ray->dir.y += (cub->direction == 'E') * 0.04;
+			cub->ray->dir.x += (cub->direction == 'S') * 0.04;
+		}
+		cub->ray->dir.x -= (cub->direction == 'N') * 0.04;
+		cub->ray->dir.y -= (cub->direction == 'W') * 0.04;
+		//printf("dir x = %f\nray dir y = %f\n", cub->ray->dir.x, cub->ray->dir.y);
+		printf("hit = %f\n", cub->ray->wall_distance);
+	}
+	else if (key.key == 83) 
+	{
+		cub->ray->dir.y -= (cub->direction == 'E') * 0.04;
+		cub->ray->dir.x -= (cub->direction == 'S') * 0.04;
+		if (cub->ray->wall_distance >= 1)
+		{
+			cub->ray->dir.x += (cub->direction == 'N') * 0.04;
+			cub->ray->dir.y += (cub->direction == 'W') * 0.04;	
+		}
+		printf("hit = %f\n", cub->ray->wall_distance);
+	}
+	else if (key.key == 68)
+	{
+		if (cub->ray->wall_distance >= 1)
+		{
+			cub->ray->dir.y += (cub->direction == 'N') * 0.04;
+			cub->ray->dir.x += (cub->direction == 'E') * 0.04;
+		}
+		cub->ray->dir.y -= (cub->direction == 'S') * 0.04;
+		cub->ray->dir.x -= (cub->direction == 'W') * 0.04;
+		printf("hit = %f\n", cub->ray->wall_distance);
+	}
+	else if (key.key == 65)
+	{
+		
+		cub->ray->dir.y -= (cub->direction == 'N') * 0.04;
+		cub->ray->dir.x -= (cub->direction == 'E') * 0.04;
+		if (cub->ray->wall_distance >= 1)
+		{
+			cub->ray->dir.y += (cub->direction == 'S') * 0.04;
+			cub->ray->dir.x += (cub->direction == 'W') * 0.04;
+		}
+		printf("hit = %f\n", cub->ray->wall_distance);
+	}
+	//printf("key = %i\n", key.key);
 }
